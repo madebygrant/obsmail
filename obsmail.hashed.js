c114ec3b07825d27d0d28898f1f95e757263c62d
madebygrant <madebygrant@protonmail.com>
2021-02-10 13:25:16 +0300

    Initial commit

---
+++ obsmail.hashed.js
@@ -0,0 +1,50 @@
+const obsMail = {
+    _d: function(encodedString){
+        let string = "",
+            keyInHex = encodedString.substr(0, 2),
+            key = parseInt(keyInHex, 16);
+    
+        for ( let n = 2; n < encodedString.length; n += 2 ) {
+            let charInHex = encodedString.substr(n, 2),
+                char = parseInt(charInHex, 16),
+                output = char ^ key;
+    
+            string += String.fromCharCode(output);
+        }
+        return string;
+    },
+
+    hashed: function(){
+        const emailLinks = document.getElementsByClassName("obs-mail");
+
+        if (0 !== emailLinks.length){
+
+            for (let i = 0; i < emailLinks.length; ++i) {
+                let emailA = emailLinks[i].getAttribute("data-link"),
+                    updated_email = this._d(emailA);
+                    label = emailLinks[i].querySelector("span");
+
+                if( label && label.innerHTML == '' ){
+                    label.textContent = updated_email;
+                }
+
+                emailLinks[i].addEventListener("click", function(e) {
+                    e.preventDefault();
+
+                    let emailA2 = updated_email,
+                        emailS = e.target.parentNode.getAttribute("data-subject");
+
+                    if (emailS != 'undefined' && emailS != null) {
+                        window.location.href = "mailto:" + emailA2 + "?subject=" + emailS
+                    } 
+                    else {
+                        window.location.href = "mailto:" + emailA2
+                    }
+
+                })
+            }
+
+        }
+    }
+}
+obsMail.hashed();
---
+++ obsmail.replace-chars.js
@@ -0,0 +1,47 @@
+/*
+function _d(char) {
+    var a = document.createElement("textarea");
+    return char.innerHTML = cahr, a.value
+}
+*/
+const obsMail = {
+    _d: function(char) {
+        let a = document.createElement("textarea");
+        return a.innerHTML = char, a.value
+    },
+
+    replaceChars: function(){
+        const emailLinks = document.getElementsByClassName("obs-mail");
+
+        if (0 !== emailLinks.length){
+
+            for (let i = 0; i < emailLinks.length; ++i) {
+                let emailA = emailLinks[i].getAttribute("data-link"),
+                    updated_email = emailA.replace(/\#/g, this._d("&#64;")).replace(/\:/g, this._d("&#46;")).replace(/\$/g, this._d("&#97;")).replace(/\%/g, this._d("&#101;")).replace(/\|/g, this._d("&#105;")).replace(/\!/g, this._d("&#111;")).replace(/\&/g, this._d("&#117;")),
+                    label = emailLinks[i].querySelector("span");
+
+                if( label && label.innerHTML == '' ){
+                    label.innerHTML = updated_email;
+                }
+
+                emailLinks[i].addEventListener("click", function(e) {
+                    e.preventDefault();
+
+                    const emailA2 = updated_email,
+                        emailS = e.target.parentNode.getAttribute("data-subject");
+
+                    if (emailS != 'undefined' && emailS != null) {
+                        window.location.href = "mailto:" + emailA2 + "?subject=" + emailS
+                    } 
+                    else {
+                        window.location.href = "mailto:" + emailA2
+                    }
+
+                })
+            }
+
+        }
+    }
+}
+
+obsMail.replaceChars();
